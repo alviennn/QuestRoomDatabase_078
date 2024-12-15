cb059271f07770dac83df0234d62808e0ec80281
alviennn <alvien.rnp@gmail.com>
2024-12-15 22:39:48 +0700

    menambahkan data class HomeUiState untuk mengelola status UI, termasuk daftar mahasiswa, status loading, error, dan pesan error

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
@@ -48,3 +48,9 @@ class HomeMhsViewModel(
         )
 }
 
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
