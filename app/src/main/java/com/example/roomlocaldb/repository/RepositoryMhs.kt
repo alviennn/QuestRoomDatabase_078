89f22ab8d4793519d154bfc8e2b53d293d51a3f6
alviennn <alvien.rnp@gmail.com>
2024-12-15 21:50:17 +0700

    menambahkan fungsi deleteMhs() untuk menghapus mahasiswa dan updateMhs() untuk memperbarui data mahasiswa

--- app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
@@ -7,4 +7,6 @@ interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMhs(): Flow<List<Mahasiswa>>
     fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
