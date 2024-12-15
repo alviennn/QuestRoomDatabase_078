500d4aaec0e91a45a8190abb86b60b0aebc4f069
alviennn <alvien.rnp@gmail.com>
2024-12-15 22:21:24 +0700

    menambahkan objek DestinasiDetail dan DestinasiUpdate dengan route dan argument NIM

--- app/src/main/java/com/example/roomlocaldb/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/navigation/AlamatNavigasi.kt
@@ -6,3 +6,15 @@ interface AlamatNavigasi {
 object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routeWithArgs = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routeWithArgs = "$route/{${NIM}"
+}
