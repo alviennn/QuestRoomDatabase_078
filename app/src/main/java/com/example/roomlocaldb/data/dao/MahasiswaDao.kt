d89acd623f10fec7e5b8d9b7be5aa041298703ca
alviennn <alvien.rnp@gmail.com>
2024-12-15 21:40:55 +0700

    fungsi untuk melakukan pembaharuan informasi mahasiswa yang sudah ada pada database

--- app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocaldb/data/dao/MahasiswaDao.kt
@@ -1,8 +1,12 @@
 package com.example.roomlocaldb.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao{
@@ -10,4 +14,9 @@ interface MahasiswaDao{
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+    @Update
+    suspend fun updateMahasiswa(
+        mahasiswa: Mahasiswa
+    )
+
 }
