c4f7930d9bf34231223374c42462d53dc6a25bcc
alviennn <alvien.rnp@gmail.com>
2024-12-15 22:12:08 +0700

    menambahkan fungsi deleteMhs(), getAllMhs(), getMhs(), dan updateMhs() untuk operasi CRUD mahasiswa dari mahasiswaDao

--- app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,24 @@ package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.dao.MahasiswaDao
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+    override fun getAllMhs(): Flow<List<Mahasiswa>> =
+        mahasiswaDao.getAllMahasiswa()
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> =
+        mahasiswaDao.getMahasiswa(nim)
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
