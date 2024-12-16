091ea62adf17dcdc83ffb7c50eaa62fd7c160011
alviennn <alvien.rnp@gmail.com>
2024-12-16 08:55:16 +0700

    menambahkan inisialisasi untuk MahasiswaViewModel, DetailMhsViewModel, dan updateMhsViewModel dengan dependensi repositoryMhs dari containerApp

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
@@ -1,15 +1,34 @@
 package com.example.roomlocaldb.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomlocaldb.KrsApp
+import com.jodifrkh.roomlocaldb.ui.viewModel.updateMhsViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            updateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
